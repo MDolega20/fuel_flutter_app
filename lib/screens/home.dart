85c1bbcd50356eaa1f25bb02b79476a30a714ccb
MDolega20 <44843530+MDolega20@users.noreply.github.com>
2019-08-05 00:30:35 +0200

    some comments

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_2/screens/page_add_data.dart';
 
+// TODo CHECK THIS https://flutter.dev/docs/catalog/samples/basic-app-bar
+
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Fuel app"),
-        ),
-        body: HomeBody());
+      appBar: AppBar(
+        title: Text("Fuel app"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.directions_car),
+            onPressed: () {
+              print("nacisnieto xd");
+//                _select(choices[0]);
+            },
+          ),
+//            menu()
+        ],
+      ),
+      body: HomeBody(),
+    );
   }
+
+//  Widget menu(){
+//    return PopupMenuButton<Choice>(
+//      onSelected: _select,
+//      itemBuilder: (BuildContext context) {
+//        return choices.skip(2).map((Choice choice) {
+//          return PopupMenuItem<Choice>(
+//            value: choice,
+//            child: Text(choice.title),
+//          );
+//        }).toList();
+//      },
+//    );
+//  }
 }
 
 class HomeBody extends StatelessWidget {
