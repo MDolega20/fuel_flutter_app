415be2970b52d0287abb265f3280818b0e3b8ff6
MDolega20 <mateusz.dolega.20@gmail.com>
2019-08-16 11:45:46 +0200

    edit item part 2

--- lib/screens/page_edit_fueling.dart
+++ lib/screens/page_edit_fueling.dart
@@ -1,4 +1,6 @@
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter_app_2/model/fuel_list_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 
@@ -19,7 +21,7 @@ class PageEditFueling extends StatelessWidget{
       appBar: AppBar(
         title: Text("Edit item id $itemIndex"),
       ),
-      body: EditFuelingBody({itemIndex: itemIndex}), //TODO something font work here :/
+      body: EditFuelingBody(itemIndex), //TODO something font work here :/
     );
   }
 }
@@ -49,6 +51,69 @@ class _EditFuelingBodyState extends State<EditFuelingBody>{
   }
 
   Widget _build(BuildContext context, FuelingListModel model){
-    return Text("Form for item $itemIndex");
+    return Text("Form for item ${widget.itemIndex}");
+  }
+
+  Widget _date(BuildContext context, FuelingListModel model){
+
+    //TODO temporary from strech
+
+    final formatDate = DateFormat("yyyy-MM-dd");
+    final formatTime = DateFormat("HH:mm");
+
+    DateTime dateStart = new DateTime.now();
+    DateTime timeStart = new DateTime.now();
+
+    return Row(
+      children: <Widget>[
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: DateTimeField(
+              format: formatDate,
+              onShowPicker: (context, currentValue) {
+                return showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1900),
+                    initialDate: currentValue ?? DateTime.now(),
+                    lastDate: DateTime(2100));
+              },
+              validator: (date) => date == null ? 'Invalid date' : null,
+              initialValue: dateStart,
+              onChanged: (date) => {
+
+              },
+              onSaved: (date) => {
+
+              },
+            ),
+          ),
+        ),
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 5),
+            child: DateTimeField(
+              format: formatTime,
+              onShowPicker: (context, currentValue) async {
+                final time = await showTimePicker(
+                  context: context,
+                  initialTime:
+                  TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+                );
+                return DateTimeField.convert(time);
+              },
+              validator: (date) => date == null ? 'Invalid date' : null,
+              initialValue: timeStart,
+              onChanged: (date) => {
+
+              },
+              onSaved: (date) => {
+
+              },
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
